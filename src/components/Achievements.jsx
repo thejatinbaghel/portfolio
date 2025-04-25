f47713124ddb21fca77e26a8be6944b851c302f4
Jatin Baghel <baghelj008@gmail.com>
2025-04-25 11:16:41 +0530

    Update files

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -55,7 +55,7 @@ const Achievements = () => {
       <div className={`bg-primary ${styles.flexCenter} ${styles.paddingX}`}>
         <div className={`${styles.boxWidth}`}>
           <h1 className="flex-1 font-poppins font-semibold ss:text-[55px] text-[45px] text-white ss:leading-[80px] leading-[80px]">
-            Achievements
+            Certifications
           </h1>
         </div>
       </div>
